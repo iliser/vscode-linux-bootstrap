7deadee19ca6667431b4437607f6c1780b85e2f1
iliser <i.d.mvmii@gmail.com>
2022-08-22 05:00:37 +0800

    feat: update erase old binaries archive

--- update.sh
+++ update.sh
@@ -4,6 +4,9 @@ set -e
 scriptDir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 p=$(pwd)/$1
 
+# delete outdated binaries archive
+find "$scriptDir" -ctime +3 -name 'code-stable.tar.gz' -exec rm -rf {} \;
+
 echo "# copy user data to tmp and remove old files"
 mv "$p/data" /tmp/code-update-data-"$1"
 
